2d88030d97fbcbd29f4ab2bc868c3af79968f7b8
ccbn <thanhtin@siliconstraits.vn>
2016-01-24 17:00:22 +0700

    update docker env for auto create db from env

--- instagram/settings.py
+++ instagram/settings.py
@@ -78,10 +78,10 @@ WSGI_APPLICATION = 'instagram.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ['DB_NAME'],
-        'USER': os.environ['DB_USER'],
-        'PASSWORD': os.environ['DB_PWD'],
-        'HOST': os.environ['DB_HOST'],
+        'NAME': os.environ['POSTGRES_DB'],
+        'USER': os.environ['POSTGRES_USER'],
+        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
+        'HOST': os.environ['POSTGRES_HOST'],
         'PORT': '5432',  # Set to empty string for default.
     }
 }
