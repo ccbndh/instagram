0e5bd43c0c691370affa6e6055ab23a60877c76d
ccbn <thanhtin@siliconstraits.vn>
2016-01-26 21:57:23 +0700

    add simple unit test

--- api_instagram/tests.py
+++ api_instagram/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
+
 # Create your tests here.
+class SimpleTestCase(TestCase):
+    def test_simple(self):
+        self.assertEquals(1 + 1, 2)
+        self.assertEquals(1 + 1, 3)
