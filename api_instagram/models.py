3efb4df0477156e188dafa199f5549e97bc0b0cb
ccbn <thanhtin@siliconstraits.vn>
2016-01-27 09:58:47 +0700

    add model Photo with one test case fail - for test travis on pull request

--- api_instagram/models.py
+++ api_instagram/models.py
@@ -1,5 +1,16 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+
+@python_2_unicode_compatible  # only if you need to support Python 2
+class Photo(models.Model):
+    user = models.ForeignKey(User, related_name="uploader")
+    title = models.CharField(max_length=120, blank=True, null=True)
+    created = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
